abb94cfcc656163cc1ac0d596b3c1de3b2fc0228
Sachin Kumar <sachin.kumar@accelerationrobotics.com>
2024-12-31 18:33:34 +0100

    code updated - check the val type - check the port names with ports - deserialize ports to get the port names

--- ras_bt_framework/behavior_template/module.py
+++ ras_bt_framework/behavior_template/module.py
@@ -80,6 +80,10 @@ class BehaviorModule(BehaviorBase,ABC):
 
     @staticmethod
     def _check_ports(decl_set:set,def_dict:dict):
+            def_vals = def_dict.values()
+            for def_val in def_vals:
+                if not isinstance(def_val,Port):
+                    raise ValueError(f"Invalid port type: {type(def_val)}")
             def_set = set(def_dict.keys())
             if not def_set.issuperset(decl_set):
                 raise ValueError(f"Missing ports: {decl_set - def_set}")
@@ -88,10 +92,14 @@ class BehaviorModule(BehaviorBase,ABC):
                 print(f"WARNING: Undeclared ports: {undecl_keys}")
                 for key in undecl_keys:
                     del def_dict[key]
+            
 
     def __post_init__(self):
         self._input_port_names = set()
         self._output_port_names = set()
+        self._input_ports = {}
+        self._output_ports = {}
+
         for _k in self.__dict__.keys():
             if _k.startswith("i_"):
                 self._input_port_names.add(_k[2:])
@@ -100,11 +108,26 @@ class BehaviorModule(BehaviorBase,ABC):
 
         if len(self._input_port_names.intersection(self._output_port_names)) > 0:
             raise ValueError(f"Duplicate ports: {self._input_port_names.intersection(self._output_port_names)}")
-        # self._check_ports(self.__class__.input_port_names,self.input_ports)
-        # self._check_ports(self.__class__.output_port_names,self.output_ports)
+        for _k, _v in self.__dict__.items():
+            if _k.startswith("i_"):
+                self._input_ports[_k[2:]] = _v
+            elif _k.startswith("o_"):
+                self._output_ports[_k[2:]] = _v
+        self._check_ports(self._input_port_names, self._input_ports)
+        self._check_ports(self._output_port_names, self._output_ports)
+
+    @staticmethod
+    def deserialize_ports(ports):
+        deserialized = {}
+        for key, value in ports.items():
+            if hasattr(value, 'serialize') and callable(getattr(value, 'serialize')):
+                deserialized[key] = value.serialize()
+            else:
+                raise AttributeError(f"The object for key '{key}' does not have a callable 'serialize' method.")
+        return deserialized
 
     def get_port_map(self):
-        return {**self.input_ports,**self.output_ports}
+        return {**self.deserialize_ports(self._input_ports),**self.deserialize_ports(self._output_ports)}
 
 @dataclass
 class BehaviorModuleCollection(Composite,BehaviorModule):
