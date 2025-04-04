d24e8f10a3b957d0d14a8a370b991076c09280cf
Ram-rajavadha02 <rar8990@gmail.com>
2025-04-04 16:22:20 +0530

    pose can be defined in centimeters

--- ras_bt_framework/behavior_utility/yaml_parser.py
+++ ras_bt_framework/behavior_utility/yaml_parser.py
@@ -1,70 +1,157 @@
 #!/usr/bin/env python3
 
-"""
-Copyright (C) 2024 Harsh Davda
+# """
+# Copyright (C) 2024 Harsh Davda
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-For inquiries or further information, you may contact:
-Harsh Davda
-Email: info@opensciencestack.org
-"""
+# For inquiries or further information, you may contact:
+# Harsh Davda
+# Email: info@opensciencestack.org
+# """
+
+# import os
+# import yaml
+# import tf_transformations
+# from .grid_parser import GridConfig
+# from ..behaviors.ports import PortPoseCfg
+# from ras_common.config.loaders.common import PoseConfig
+
+
+# def read_yaml_to_pose_dict(path):
+#     with open(path, 'r') as file:
+#         data = yaml.safe_load(file)
+    
+#     if 'Poses' not in data:
+#         raise KeyError("The key 'poses' is missing from the YAML file.")
+    
+#     pose_dict = {}
+#     for pose_name, pose_values in data['Poses'].items():
+#         # qx, qy, qz, qw = tf_transformations.quaternion_from_euler(pose_values['roll'], pose_values['pitch'], pose_values['yaw'])
+#         # pose_dict[pose_name] = ",".join(map(str,[
+#         #     pose_values['x'],
+#         #     pose_values['y'],
+#         #     pose_values['z'],
+#         #     qx, qy, qz, qw
+#         # ]))
+#         pose_dict[pose_name] = PortPoseCfg(pose=PoseConfig.from_dict(pose_values))
+
+#     if 'targets' not in data:
+#         raise KeyError("The key 'targets' is missing from the YAML file.")
+#     grid_dict = {}
+#     if "Grids" in data:
+#         for grid_name, grid_values in data['Grids'].items():
+#             grid_dict[grid_name] = GridConfig.from_dict(grid_values)
+#     # target_pose = []
+
+#     # for i in data["targets"]:
+#     #     if i == "grasp":
+#     #         target_pose.append(True)
+#     #     elif i == "release":
+#     #         target_pose.append(False)
+#     #     elif type(i) == float:
+#     #         target_pose.append(i)
+#     #     else:
+#     #         for j in pose_dict:
+#     #             if j == i:
+#     #                 target_pose.append(pose_dict[j])
+
+#     return pose_dict,data["targets"],grid_dict
 
 import os
 import yaml
-import tf_transformations
-from .grid_parser import GridConfig
 from ..behaviors.ports import PortPoseCfg
 from ras_common.config.loaders.common import PoseConfig
+from ras_common.config.loaders.lab_setup import LabSetup
+def validate_pose_values(pose_values):
+    """
+    Validate pose values against robot-specific constraints.
+    Args:
+        pose_values (dict): Dictionary containing pose values with keys 'x', 'y', 'z', 'roll', 'pitch', 'yaw'
+    Raises:
+        ValueError: If any pose value is out of range
+    """
+    # Initialize LabSetup if not already initialized
+    LabSetup.init()
+    # Get the robot constraints
+    if LabSetup.constraints is None:
+        raise ValueError(f"No constraints found for robot {LabSetup.robot_name}")
+    workspace_constraints = LabSetup.constraints.workspace
+    orientation_constraints = LabSetup.constraints.orientation
+    for key, value in pose_values.items():
+        if key in workspace_constraints:
+            min_val, max_val = workspace_constraints[key]
+        elif key in orientation_constraints:
+            min_val, max_val = orientation_constraints[key]
+        else:
+            raise ValueError(f"Unknown constraint key: {key}")
+        if not (min_val <= value <= max_val):
+            raise ValueError(
+                f"Invalid value for {key}: {value}. Must be between {min_val} and {max_val} for {LabSetup.robot_name} robot"
+            )
 
+def convert_pose_to_meters(pose_values):
+    """
+    Converts pose values from centimeters to meters.
+
+    Args:
+        pose_values (dict): A dictionary containing pose values in centimeters.
+
+    Returns:
+        dict: A dictionary with pose values converted to meters.
+    """
+    return {
+        'x': pose_values['x'] / 100,  # Convert cm to meters
+        'y': pose_values['y'] / 100,
+        'z': pose_values['z'] / 100,
+        'roll': pose_values['roll'],  # Assuming roll, pitch, yaw are in radians
+        'pitch': pose_values['pitch'],
+        'yaw': pose_values['yaw']
+    }
 
 def read_yaml_to_pose_dict(path):
+    print(f"Reading YAML file: {path}")
     with open(path, 'r') as file:
         data = yaml.safe_load(file)
-    
     if 'Poses' not in data:
         raise KeyError("The key 'poses' is missing from the YAML file.")
-    
     pose_dict = {}
     for pose_name, pose_values in data['Poses'].items():
-        # qx, qy, qz, qw = tf_transformations.quaternion_from_euler(pose_values['roll'], pose_values['pitch'], pose_values['yaw'])
-        # pose_dict[pose_name] = ",".join(map(str,[
-        #     pose_values['x'],
-        #     pose_values['y'],
-        #     pose_values['z'],
-        #     qx, qy, qz, qw
-        # ]))
-        pose_dict[pose_name] = PortPoseCfg(pose=PoseConfig.from_dict(pose_values))
 
+        # Convert pose values from centimeters to meters
+        pose_values = convert_pose_to_meters(pose_values)
+
+        validate_pose_values(pose_values)
+        pose_dict[pose_name] = PortPoseCfg(pose=PoseConfig.from_dict(pose_values))
     if 'targets' not in data:
         raise KeyError("The key 'targets' is missing from the YAML file.")
-    grid_dict = {}
-    if "Grids" in data:
-        for grid_name, grid_values in data['Grids'].items():
-            grid_dict[grid_name] = GridConfig.from_dict(grid_values)
-    # target_pose = []
-
-    # for i in data["targets"]:
-    #     if i == "grasp":
-    #         target_pose.append(True)
-    #     elif i == "release":
-    #         target_pose.append(False)
-    #     elif type(i) == float:
-    #         target_pose.append(i)
-    #     else:
-    #         for j in pose_dict:
-    #             if j == i:
-    #                 target_pose.append(pose_dict[j])
-
-    return pose_dict,data["targets"],grid_dict
+    target_pose = []
+    for action in data["targets"]:
+        if isinstance(action, dict):
+            key = list(action.keys())[0]
+            value = action[key]
+            if key == "move2pose":
+                if value in pose_dict:
+                    target_pose.append({key: value})
+                else:
+                    raise KeyError(f"Undefined pose '{value}' in 'targets' section. Available poses: {list(pose_dict.keys())}")
+            elif key == "gripper":
+                target_pose.append({key: bool(value)})
+            elif key == "rotate":
+                target_pose.append({key: float(value)})
+            else:
+                raise KeyError(f"Unknown target action: {key}")
+        else:
+            raise ValueError("Invalid format in 'targets'. Each entry must be a dictionary.")
+    return pose_dict, target_pose
--- ras_bt_framework/behaviors/keywords.py
+++ ras_bt_framework/behaviors/keywords.py
@@ -1,40 +1,170 @@
-"""
-Copyright (C) 2024 Harsh Davda
+# """
+# Copyright (C) 2024 Harsh Davda
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-For inquiries or further information, you may contact:
-Harsh Davda
-Email: info@opensciencestack.org
-"""
+# For inquiries or further information, you may contact:
+# Harsh Davda
+# Email: info@opensciencestack.org
+# """
+
+# from ..behavior_template.module import BehaviorModuleSequence
+# from ..behaviors.primitives import MoveToPose,RotateEffector,Trigger, MoveToJointState
+# from ..behaviors.modules import PickSequence,PlaceSequence
+# from typing import List,Dict
+# from ..behavior_utility.grid_parser import GridConfig
+# from ras_common.config.loaders.objects import ObjectTypes
+# from .ports import PortPoseCfg
+# from copy import deepcopy
+
+# class TargetPoseMap(object):
+#     def __init__(self):
+#         self.pose_map = {}
+    
+#     def register_pose(self,pose_name,pose):
+#         self.pose_map[pose_name] = pose
+    
+#     def move2pose_module(self,pose:str):
+#         if (isinstance(pose,str)):
+#             if pose in self.pose_map:
+#                 return MoveToPose(i_pose=self.pose_map[pose])
+#             else:
+#                 raise ValueError(f"Invalid pose name {pose}")
+#         else:
+#             raise ValueError(f"Invalid pose input type {type(pose)}")
+        
+#     def move2pose_sequence_module(self,poses:List[str]):
+#         move2pose_sequence = BehaviorModuleSequence()
+#         move2pose_sequence.add_children([self.move2pose_module(pose) for pose in poses])
+#         return move2pose_sequence
+
+#     def pick_module(self,pose:str,clearance:float=0.10,height:float=0.10):
+#         if pose in self.pose_map:
+#             return PickSequence(pose,clearance=clearance,height=height)
+#         else:
+#             raise ValueError(f"Invalid pose name {pose}")
+    
+#     def place_module(self,pose:str,clearance:float=0.10,height:float=0.10):
+#         if pose in self.pose_map:
+#             return PlaceSequence(pose,clearance=clearance,height=height)
+#         else:
+#             raise ValueError(f"Invalid pose name {pose}")
+        
+    
+
+# class GridLocationMap(object):
+#     def __init__(self):
+#         self.grid_pose_map : Dict[str,TargetPoseMap] = {}
+#         # self.stack_height = stack_height
+    
+#     def register_grid(self,grid_name,grid:GridConfig):
+#         pose_map = TargetPoseMap()
+        
+#         for location_name,location in grid.locations.items():
+#             pose_config = deepcopy(grid.pose)
+#             pose_config.x += location.x
+#             pose_config.y += location.y
+#             pose = PortPoseCfg(pose=pose_config)
+#             pose_map.register_pose(location_name,pose)
+#         self.grid_pose_map[grid_name] = pose_map
+    
+#     def move2location(self,grid:str,location:str):
+#         if grid in self.grid_pose_map:
+#             return self.grid_pose_map[grid].move2pose_module(location)
+#         else:
+#             raise ValueError(f"Invalid grid name {grid}")
+#     def move2location_sequence(self,grid:str,locations:List[str]):
+#         if grid in self.grid_pose_map:
+#             return self.grid_pose_map[grid].move2pose_sequence_module(locations)
+#         else:
+#             raise ValueError(f"Invalid grid name {grid}")
+    
+#     def pick_location(self,object:str,grid:str,location:str,level:int=0,clearance:float=0.07,height:float=0.07):
+#         if grid in self.grid_pose_map:
+#             pose_map = self.grid_pose_map[grid].pose_map
+#             if location in pose_map:
+#                 ObjectTypes.init()
+#                 object_config = ObjectTypes.get_object(object)
+#                 pose : PortPoseCfg = deepcopy(pose_map[location])
+#                 height_delta = object_config.interaction_height + (level*object_config.max_height)
+#                 pose.pose.z += height_delta
+#                 return PickSequence(pose,clearance=clearance,height=height)
+#             else:
+#                 raise ValueError(f"Invalid location name {location}")
+#         else:
+#             raise ValueError(f"Invalid grid name {grid}")
+    
+#     def place_location(self,object:str,grid:str,location:str,level:int=0,clearance:float=0.07):
+#         if grid in self.grid_pose_map:
+#             pose_map = self.grid_pose_map[grid].pose_map
+#             if location in pose_map:
+#                 ObjectTypes.init()
+#                 object_config = ObjectTypes.get_object(object)
+#                 pose : PortPoseCfg = deepcopy(pose_map[location])
+#                 height_delta = object_config.interaction_height + (level*object_config.max_height)
+#                 pose.pose.z += height_delta
+#                 return PlaceSequence(pose,clearance=clearance)
+#             else:
+#                 raise ValueError(f"Invalid location name {location}")
+#         else:
+#             raise ValueError(f"Invalid grid name {grid}")
+
+# def rotate(angle:float):
+#     return RotateEffector(i_rotation_angle=angle)
+
+# def gripper(open:bool):
+#     return Trigger(i_trigger=open)
+
+# # def home_joint_state():
+# #     from ras_common.config.loaders.lab_setup import LabSetup
+# #     LabSetup.init()
+# #     return MoveToJointState(i_joint_names=','.join(map(str,LabSetup.conf.robot.home_joint_state.keys())),i_joint_values=','.join(map(str,LabSetup.conf.robot.home_joint_state.values())))
+
+# def joint_state(joints:list):
+#     from ras_common.config.loaders.lab_setup import LabSetup
+#     LabSetup.init()
+#     joint_names = list(LabSetup.conf.robot.home_joint_state.keys())
+#     if len(joints) != len(joint_names): 
+#         raise ValueError(f"Invalid number of joints {len(joints)}")
+#     joint_state = ",".join([f"{joint_names[i]}:{joints[i]}" for i in range(len(joints))])
+#     return MoveToJointState(i_joint_state=joint_state)
+
+# # def pick_object(object_name:str,dst:str):
+# #     src = TargetPoseMap().pose_map[object_name]
+# #     return BehaviorModuleSequence(children=[MoveToPose(input_ports={"pose":src}),
+# #                                             Trigger(input_ports={"trigger":"True"}),
+# #                                             MoveToPose(input_ports={"pose":dst}),
+# #                                             Trigger(input_ports={"trigger":"False"})])
+
+# keyword_mapping = {
+#             "rotate":rotate,
+#             "gripper":gripper,
+#             'joint_state': joint_state
+#             }
 
 from ..behavior_template.module import BehaviorModuleSequence
 from ..behaviors.primitives import MoveToPose,RotateEffector,Trigger, MoveToJointState
 from ..behaviors.modules import PickSequence,PlaceSequence
 from typing import List,Dict
-from ..behavior_utility.grid_parser import GridConfig
 from ras_common.config.loaders.objects import ObjectTypes
 from .ports import PortPoseCfg
 from copy import deepcopy
-
 class TargetPoseMap(object):
     def __init__(self):
         self.pose_map = {}
-    
     def register_pose(self,pose_name,pose):
         self.pose_map[pose_name] = pose
-    
     def move2pose_module(self,pose:str):
         if (isinstance(pose,str)):
             if pose in self.pose_map:
@@ -43,112 +173,44 @@ class TargetPoseMap(object):
                 raise ValueError(f"Invalid pose name {pose}")
         else:
             raise ValueError(f"Invalid pose input type {type(pose)}")
-        
     def move2pose_sequence_module(self,poses:List[str]):
         move2pose_sequence = BehaviorModuleSequence()
         move2pose_sequence.add_children([self.move2pose_module(pose) for pose in poses])
         return move2pose_sequence
-
     def pick_module(self,pose:str,clearance:float=0.10,height:float=0.10):
         if pose in self.pose_map:
             return PickSequence(pose,clearance=clearance,height=height)
         else:
             raise ValueError(f"Invalid pose name {pose}")
-    
     def place_module(self,pose:str,clearance:float=0.10,height:float=0.10):
         if pose in self.pose_map:
             return PlaceSequence(pose,clearance=clearance,height=height)
         else:
             raise ValueError(f"Invalid pose name {pose}")
-        
-    
-
-class GridLocationMap(object):
-    def __init__(self):
-        self.grid_pose_map : Dict[str,TargetPoseMap] = {}
-        # self.stack_height = stack_height
-    
-    def register_grid(self,grid_name,grid:GridConfig):
-        pose_map = TargetPoseMap()
-        
-        for location_name,location in grid.locations.items():
-            pose_config = deepcopy(grid.pose)
-            pose_config.x += location.x
-            pose_config.y += location.y
-            pose = PortPoseCfg(pose=pose_config)
-            pose_map.register_pose(location_name,pose)
-        self.grid_pose_map[grid_name] = pose_map
-    
-    def move2location(self,grid:str,location:str):
-        if grid in self.grid_pose_map:
-            return self.grid_pose_map[grid].move2pose_module(location)
-        else:
-            raise ValueError(f"Invalid grid name {grid}")
-    def move2location_sequence(self,grid:str,locations:List[str]):
-        if grid in self.grid_pose_map:
-            return self.grid_pose_map[grid].move2pose_sequence_module(locations)
-        else:
-            raise ValueError(f"Invalid grid name {grid}")
-    
-    def pick_location(self,object:str,grid:str,location:str,level:int=0,clearance:float=0.07,height:float=0.07):
-        if grid in self.grid_pose_map:
-            pose_map = self.grid_pose_map[grid].pose_map
-            if location in pose_map:
-                ObjectTypes.init()
-                object_config = ObjectTypes.get_object(object)
-                pose : PortPoseCfg = deepcopy(pose_map[location])
-                height_delta = object_config.interaction_height + (level*object_config.max_height)
-                pose.pose.z += height_delta
-                return PickSequence(pose,clearance=clearance,height=height)
-            else:
-                raise ValueError(f"Invalid location name {location}")
-        else:
-            raise ValueError(f"Invalid grid name {grid}")
-    
-    def place_location(self,object:str,grid:str,location:str,level:int=0,clearance:float=0.07):
-        if grid in self.grid_pose_map:
-            pose_map = self.grid_pose_map[grid].pose_map
-            if location in pose_map:
-                ObjectTypes.init()
-                object_config = ObjectTypes.get_object(object)
-                pose : PortPoseCfg = deepcopy(pose_map[location])
-                height_delta = object_config.interaction_height + (level*object_config.max_height)
-                pose.pose.z += height_delta
-                return PlaceSequence(pose,clearance=clearance)
-            else:
-                raise ValueError(f"Invalid location name {location}")
-        else:
-            raise ValueError(f"Invalid grid name {grid}")
-
 def rotate(angle:float):
     return RotateEffector(i_rotation_angle=angle)
-
 def gripper(open:bool):
     return Trigger(i_trigger=open)
-
-# def home_joint_state():
-#     from ras_common.config.loaders.lab_setup import LabSetup
-#     LabSetup.init()
-#     return MoveToJointState(i_joint_names=','.join(map(str,LabSetup.conf.robot.home_joint_state.keys())),i_joint_values=','.join(map(str,LabSetup.conf.robot.home_joint_state.values())))
-
 def joint_state(joints:list):
+    """
+    Create a MoveToJointState behavior with the given joint values.
+    Args:
+        joints (list): List of joint values in radians. Must match the number of joints for the active robot.
+    Returns:
+        MoveToJointState: A behavior node for moving to the specified joint state.
+    Raises:
+        ValueError: If the number of joints doesn't match the active robot's configuration.
+    """
     from ras_common.config.loaders.lab_setup import LabSetup
     LabSetup.init()
-    joint_names = list(LabSetup.conf.robot.home_joint_state.keys())
-    if len(joints) != len(joint_names): 
-        raise ValueError(f"Invalid number of joints {len(joints)}")
+    # Get joint names from the active robot's configuration
+    joint_names = list(LabSetup.robot_config.home_joint_state.keys())
+    if len(joints) != len(joint_names):
+        raise ValueError(f"Invalid number of joints {len(joints)} for robot {LabSetup.robot_name}. Expected {len(joint_names)} joints.")
     joint_state = ",".join([f"{joint_names[i]}:{joints[i]}" for i in range(len(joints))])
     return MoveToJointState(i_joint_state=joint_state)
-
-# def pick_object(object_name:str,dst:str):
-#     src = TargetPoseMap().pose_map[object_name]
-#     return BehaviorModuleSequence(children=[MoveToPose(input_ports={"pose":src}),
-#                                             Trigger(input_ports={"trigger":"True"}),
-#                                             MoveToPose(input_ports={"pose":dst}),
-#                                             Trigger(input_ports={"trigger":"False"})])
-
 keyword_mapping = {
-            "rotate":rotate,
-            "gripper":gripper,
-            'joint_state': joint_state
-            }
+    "rotate": rotate,
+    "gripper": gripper,
+    'joint_state': joint_state
+}
